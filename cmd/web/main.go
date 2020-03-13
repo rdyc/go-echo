16343ef2c57ffbe18c628aaf732aadb96255a1bb
Ruddy Cahyadi <ruddycahyadi@gmail.com>
2020-03-13 16:26:32 +0700

    change public path

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -60,9 +60,8 @@ func main() {
 	}))
 
 	// web router
-	e.File("/", "../../public/index.html")
-	//e.File("/favicon.ico", "../../public/assets/favicon/favicon.ico")
-	e.Static("/assets", "../../public/assets")
+	e.File("/", "../public/index.html")
+	e.Static("/assets", "../public/assets")
 
 	// api router group
 	v1 := e.Group("/v1")
